42972e825da8e64a9e84e9736e68b604814591b6
simon <simon.quemeneur@capgemini.com>
2017-12-15 23:51:51 +0100

    Ajout du projet Console de test

---
+++ BookStore.ConsoleTestApp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.ConsoleTestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Démarrage de l'application Console");
+            try
+            {
+                using (var ctx = new CoreLibrary.BookStoreEntities())
+                {
+                    foreach (var p in ctx.Persons)
+                    {
+                        Console.WriteLine(p.LastName);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.Error.Write("Une erreur est survenue :");
+                Console.Error.WriteLine(ex);
+            }
+            Console.WriteLine("Appuyez sur une touche pour quitter");
+            Console.ReadKey();
+        }
+    }
+}
